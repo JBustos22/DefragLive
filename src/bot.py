6b2f1159e1313c6acb1296cd4f71d2c351c10918
Donaldduck8 <donaldgit@yandex.com>
2020-10-22 19:20:46 +0200

    Fix command injection protection

--- src/bot.py
+++ src/bot.py
@@ -35,7 +35,8 @@ async def event_message(ctx):
 
     # bot.py, at the bottom of event_message
     if message.startswith("!"):
-        message = message[:message.index(";")]
+        if ";" in message:
+            message = message[:message.index(";")]
         split_msg = message.strip('!').split(' ')
         cmd = split_msg[0]
         args = split_msg[1:] if len(split_msg) > 0 else None
