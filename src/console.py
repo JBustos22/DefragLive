b72726cf754e9a1c1ab931bb0ad9592195a3479e
JBustos22 <jorrgebustos@gmail.com>
2020-10-29 21:15:44 -0400

    added some basic commands for proof of concept

--- src/console.py
+++ src/console.py
@@ -1,6 +1,5 @@
 import time
 import api as api
-import config
 from termcolor import colored
 
 
@@ -23,5 +22,14 @@ def read(file_path, verbose=True):
 
 def process_line(line):
     # RE stuff, calls command functions go here
-    if "?nospec" in line: # switch this to a RE match
-        api.exec_command(f"say Nospec activated!")
+    # this is proof of concept, we should find a way not to have a bunch of if statements
+    if ": ^2?nospec" in line: # switch this to a RE match
+        # no spec logic or function with logic here
+        api.exec_command(f"say Nospec command received.")
+    elif ": ^2?info" in line:
+        info_string = "This is a 24/7 livestream: https://www.twitch.tv/defraglive. Contact: defragtv@gmail.com. " \
+                      "Use ?help for all the available commands"
+        api.exec_command(f"say {info_string}")
+    elif ": ^2?help" in line:
+        help_string = "Current commands are ?nospec, ?info, and ?help"
+        api.exec_command(f"say {help_string}")
