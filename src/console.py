511458f0e096dd4595d2a516856bcf91e4bf5d62
Donaldduck8 <donaldgit@yandex.com>
2020-10-30 13:46:33 +0100

    WIP process_line()

--- src/console.py
+++ src/console.py
@@ -1,10 +1,16 @@
+import os
 import time
 import api as api
 from termcolor import colored
 import commands
+import traceback
+import re
 
 
 def read(file_path, verbose=True):
+    while not os.path.isfile(file_path):
+        time.sleep(1)
+
     with open(file_path, 'r') as log:
         while 1:
             where = log.tell()
@@ -13,39 +19,88 @@ def read(file_path, verbose=True):
                 time.sleep(1)
                 log.seek(where)
             elif "serverCommand" in line:
-                if verbose:
-                    print(colored(line, 'yellow'))
                 line_data = process_line(line)
-                if bool(line_data):  # check if line is a command TODO: Use some something like bool(line_data.command)
-                    process_command(line_data, "use line_data.command here")
-                else:
-                    ""
-                    print("Not a command")
-                    # something else
+
+                if line_data["type"] == "SAY":
+                    process_command(line_data, line_data["content"])
+
 
 
 def process_line(line):
-    # TODO: Add formatting logic here. RE stuff should go here. Format into a nice dictionary like this
+    line = line.strip()
 
-    # formatting...
-    # line_data = {"type": line_type, "command": command, "author": author, "content": content, ...}
-    # return line_data
-    return line
+    line_data = {"type" : "MISC", "command" : None, "author" : None, "content" : line}
+
+    # SERVERCOMMAND
+    try:
+        sc_r = r"^\^5serverCommand:\s*(\d+?)\s*:\s*(.+?)$"
+        match = re.match(sc_r, line)
+
+        command_id = match.group(1)
+        command = match.group(2)
+
+        line_data["command"] = command
+
+        def parse_chat_message(command):
+            # CHAT MESSAGE (BY PLAYER)
+            chat_message_r = r"^chat\s*\"[\x19]*\[*(.*?)[\x19]*?\]*?\x19:\s*(.*?)\".*?$"
+            match = re.match(chat_message_r, command)
+
+            chat_name = match.group(1)
+            chat_message = match.group(2)
+
+            line_data["type"] = "SAY"
+            line_data["author"] = chat_name
+            line_data["content"] = chat_message
+
+        def parse_chat_announce(command):
+            # CHAT ANNOUNCEMENT
+            chat_announce_r = r"^chat\s*\"(.*?)\".*?$"
+            match = re.match(chat_announce_r, command)
+
+            chat_announcement = chat_announce_r.group(1)
+
+            line_data["type"] = "ANNOUNCE"
+            line_data["author"] = None
+            line_data["content"] = chat_announcement
+
+        def parse_print(command):
+            # PRINT
+            print_r = r"^print\s*\"(.*?)$" # Prints have their ending quotation mark on the next line, very strange
+            match = re.match(print_r, command)
+
+            print_message = match.group(1)
+
+            line_data["type"] = "PRINT"
+            line_data["author"] = None
+            line_data["content"] = print_message
+
+        for fun in [parse_chat_message, parse_chat_announce, parse_print]:
+            try:
+                fun(command)
+                break
+            except:
+                continue
+    except:
+        return line_data
+
+    print(line_data)
+    return line_data
 
 
-def process_command(line_data, command):
+def process_command(line_data, message):
     # TODO: Should find a way not to have a bunch of if statements
     # TODO: Use command functions from command.py to process for each situation
 
     # Proof of concept code:
     # TODO: Change conditionals to use command param, etc.
-    if ": ^2?nospec" in line_data: # switch this to a RE match
+    if "?nospec" in message: # switch this to a RE match
         # no spec logic or function with logic here
         api.exec_command(f"say Nospec command received.")
-    elif ": ^2?info" in line_data:
+    elif "?info" in message:
         info_string = "This is a 24/7 livestream: https://www.twitch.tv/defraglive. Contact: defragtv@gmail.com. " \
                       "Use ?help for all the available commands"
         api.exec_command(f"say {info_string}")
-    elif ": ^2?help" in line_data:
+    elif "?help" in message:
         help_string = "Current commands are ?nospec, ?info, and ?help"
         api.exec_command(f"say {help_string}")
