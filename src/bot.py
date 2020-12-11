6ea3f9c4f1a1ff8145b147109b45354eaf791d1a
neyoneit <neyoneit@gmail.com>
2020-12-11 23:31:28 +0100

    frog is mean

--- src/bot.py
+++ src/bot.py
@@ -16,8 +16,6 @@ import asyncio
 import websockets
 import json
 
-# neit is dumb
-
 df_channel = environ['CHANNEL'] if 'CHANNEL' in environ and environ['CHANNEL'] != "" else input("Your twitch channel name: ")
 
 # bot setup
