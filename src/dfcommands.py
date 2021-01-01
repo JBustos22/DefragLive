720ad5c3316fec8eb8a23706f3248150e0b22d55
neyoneit <neyoneit@gmail.com>
2021-01-01 15:21:42 +0100

    small changes with ingame commands

--- src/dfcommands.py
+++ src/dfcommands.py
@@ -1,7 +1,7 @@
 """This file contains all the handling logic for each twitchbot command available to DeFRaG players"""
 
 import api
-supported_commands = ["nospec", "info", "help", "viewers", "clear"]
+supported_commands = ["nospec", "info", "help", "howmany", "clear"]
 
 
 def scan_for_command(message):
@@ -19,7 +19,7 @@ def scan_for_command(message):
 # The following are all the handler functions. They each take in line_data and return None
 
 def handle_help(line_data):
-    reply_string = "^7Current commands are ^3?^7nospec, ^3?^7info, ^3?^7help, ^3?^7clear, and ^3?^7viewers"
+    reply_string = "^7Current commands are ^3?^7nospec, ^3?^7info, ^3?^7help, ^3?^7clear, and ^3?^7howmany"
     api.exec_command(f"say {reply_string}")
     return None
 
@@ -30,21 +30,22 @@ def handle_nospec(line_data):
 
 
 def handle_info(line_data):
-    reply_string = "^7This is a ^324/7 ^7livestream: ^3https://defrag.tv ^7| Contact: ^3defragtv@gmail.com. " \
-                  "^7| Use ^3?^7help for a list of commands"
-    api.exec_command(f"say {reply_string}")
+    reply_string_1 = "^7This is a ^324/7 ^7livestream: ^3https://defrag.tv ^7| Contact: ^3defragtv@gmail.com."
+    reply_string_2 = "^7Use ^3?^7help for a list of commands"
+    api.exec_command(f"say {reply_string_1}")
+    api.exec_command(f"say {reply_string_2}")
     return None
 
 
-def handle_viewers(line_data):
+def handle_howmany(line_data):
     import serverstate
-    viewer_count = 5
-    reply_string = f"^1{serverstate.STATE.current_player.n}^7, you are being watched by ^3{viewer_count} ^7viewer" + ("s" if viewer_count > 0 else "")
-    api.exec_command(f"say {reply_string}")
+    viewer_count = 69
+    reply_string = f"$chsinfo(117) ^7you are being watched by ^3{viewer_count} ^7viewer" + ("s" if viewer_count > 0 else "")
+    api.exec_command(f"varcommand say {reply_string}")
     return None
 
 
 def handle_clear(line_data):
-   reply_string = "^7Ingame chat for viewers on stream has been ^1erased."
+   reply_string = "^7Ingame chat for viewers has been ^1erased."
    api.exec_command(f"clear; say {reply_string}")
    return None
