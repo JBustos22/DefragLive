8647689665cf182d9421cf6140dcd7b6b38ebd80
JBustos22 <jorrgebustos@gmail.com>
2021-04-24 18:08:36 -0400

    fix log destination

--- src/bot.py
+++ src/bot.py
@@ -17,6 +17,7 @@ from multiprocessing import Process
 import logging
 from datetime import datetime
 import sys
+import pathlib
 
 df_channel = environ['CHANNEL'] if 'CHANNEL' in environ and environ['CHANNEL'] != "" else input("Your twitch channel name: ")
 
@@ -393,9 +394,9 @@ def ws_worker(q, loop):
 if __name__ == "__main__":
     config.read_cfg()
     window_flag = False
-
-    twitchbot_logfile = __file__.replace("src/bot.py", f'logs/{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}_twitchbot.log')
-    file_handler = logging.FileHandler(filename=twitchbot_logfile)
+    twitchbot_logfile =f'{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}_twitchbot.log'
+    twitchbot_logpath = list(pathlib.Path(__file__).parent.parent.parts) + ['logs'] + [twitchbot_logfile]
+    file_handler = logging.FileHandler(filename=os.path.join(*twitchbot_logpath))
     stdout_handler = logging.StreamHandler(sys.stdout)
     handlers = [file_handler, stdout_handler]
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S', level=logging.INFO, handlers=handlers)
